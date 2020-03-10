f93f533a3fc68b4d554543394a7ff674a09bf0a2
Husnain <husnain.butt300@gmail.com>
2020-03-10 18:49:02 +0500

    validations done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  form:FormGroup;
+  isFormSubmitted:boolean;
+  constructor( private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.isFormSubmitted=false;
+   this.createForm();
+  }
+  createForm(){
+    this.form = this.formBuilder.group(
+      {
+        name: ['', Validators.required],
+        password: ['', Validators.required],
+        
+      })
+  }
+  get f() { return this.form.controls; }
+  onSubmit(): void {
+    this.isFormSubmitted=true;
+    if(this.form.errors){
+      return;
+    }
+    console.log(this.form.value);
   }
 
 }
